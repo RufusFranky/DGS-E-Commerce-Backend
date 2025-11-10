7fd174c9aab93223bb71ce0796b16908ddf0f4c5
RufusFranky <151731451+RufusFranky@users.noreply.github.com>
2025-11-10 16:43:04 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// ✅ Use only the connection string in production
+//Use only the connection string in production
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }, // required for Neon DB
